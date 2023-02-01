c310ca4bb59647fb799984294b46fccf5a6665f0
harrithha <harrithha64@gmail.com>
2023-02-01 18:27:02 +0530

    Changed underscore

--- minix/servers/pm/forkexit.c
+++ minix/servers/pm/forkexit.c
@@ -116,7 +116,7 @@ int do_fork()
   /* Find a free pid for the child and put it in the table. */
   new_pid = get_free_pid();
   rmc->mp_pid = new_pid; /* assign pid to child */
-  printf("Minix: PID %d created", rmc->mp pid);
+  printf("Minix: PID %d created", rmc->mp_pid);
   memset(&m, 0, sizeof(m));
   m.m_type = VFS_PM_FORK;
   m.VFS_PM_ENDPT = rmc->mp_endpoint;
@@ -251,7 +251,7 @@ int do_exit()
   else
   {
     exit_proc(mp, m_in.m_lc_pm_exit.status, FALSE /*dump_core*/);
-    printf("Minix: PID %d exited", mp->mp pid);
+    printf("Minix: PID %d exited", mp->mp_pid);
   }
   return (SUSPEND); /* can't communicate from beyond the grave */
 }
